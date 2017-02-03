8dfaecde81f7c347a3c1e2e51eba8fa5616f0458
Bora Oztekin <boramoztekin@gmail.com>
2017-02-03 11:28:38 -0600

    List names of maps

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-$(function() {
-  var $list = $("div.dropdown-content");
-  var sorted = $("a", $list).toArray().sort(function(a, b) {
-    return b.textContent[0] < a.textContent[0]
-  });
-  $list.html(sorted);
+$(function () {
+    var $list = $("div.dropdown-content");
+    var sorted = $("a", $list).toArray().sort(function (a, b) {
+        return b.textContent[0] < a.textContent[0]
+    });
+    $list.html(sorted);
+})
+
+$(function () {
+    var $list = $("ul.map_selector");
+    var sorted = $("li h3", $list).toArray().sort(function (a, b) {
+        return b.textContent[0] < a.textContent[0]
+    });
+    $list.html(sorted);
 })
