3aa447859c579e4d4b73c2edc8ab29fd4899f029
Bora Oztekin <boramoztekin@gmail.com>
2017-02-02 20:38:25 -0600

    Amend reordered list items

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
-$(document).ready(function() {
-        var $sort = this;
-        var $list = $('div.dropdown-content');
-        var $listLi = $('a',$list);
-        $listLi.sort(function(a, b){
-            var keyA = $(a).text();
-            var keyB = $(b).text();
-            if($($sort).hasClass('asc')){
-                return (keyA > keyB) ? 1 : 0;
-            } else {
-                return (keyA < keyB) ? 1 : 0;
-            }
-        });
-        $.each($listLi, function(index, row){
-            $list.append(row);
-        });
-        e.preventDefault();
+$(document).ready(function (e) {
+    var $sort = this;
+    var $list = $('div.dropdown-content');
+    var $listLi = $('div.dropdown-content a', $list);
+    $listLi.sort(function (a, b) {
+        var keyA = $(a).text();
+        var keyB = $(b).text();
+        if ($($sort).hasClass('asc')) {
+            return (keyA > keyB) ? 1 : 0;
+        } else {
+            return (keyA < keyB) ? 1 : 0;
+        }
     });
+    $.each($listLi, function (index, row) {
+        $list.append(row);
+    });
+});
