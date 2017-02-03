4a816b35ada32cf32bc243cf3fc0f061a00875f9
Bora Oztekin <boramoztekin@gmail.com>
2017-02-02 22:46:10 -0600

    Simplify sorting function

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
-$(document).ready(function (e) {
-    var $sort = this;
-    var $list = $('div.dropdown-content');
-    var $listLi = $('a', $list);
-    $listLi.sort(function (a, b) {
-        var keyA = $(a).text();
-        var keyB = $(b).text();
-        return (keyA > keyB) ? 1 : 0;
-    });
-    $.each($listLi, function (index, row) {
-        $list.append(row);
-    });
-});
+$(function() {
+  var $list = $("div.dropdown-content");
+  var sorted = $("a", $list).toArray().sort(function(a, b) {
+    return b.textContent[0] < a.textContent[0]
+  });
+  $list.html(sorted);
+})
