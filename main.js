d79e021ba6c88091cde5a680028727581f49ac55
Bora Oztekin <boramoztekin@gmail.com>
2017-02-02 20:54:02 -0600

    Patch for descending list (instead of ascending)

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 $(document).ready(function (e) {
     var $sort = this;
     var $list = $('div.dropdown-content');
-    var $listLi = $('div.dropdown-content a', $list);
+    var $listLi = $('a', $list);
     $listLi.sort(function (a, b) {
         var keyA = $(a).text();
         var keyB = $(b).text();
         if ($($sort).hasClass('asc')) {
             return (keyA > keyB) ? 1 : 0;
         } else {
-            return (keyA < keyB) ? 1 : 0;
+            return (keyA > keyB) ? 1 : 0;
         }
     });
     $.each($listLi, function (index, row) {
