7f5ba379dcb30e52298651e1f13a72096acfc9eb
Bora Oztekin <boramoztekin@gmail.com>
2017-02-02 20:20:38 -0600

    Add JavaScript sorter to alphabetize map dropdown

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+        var $sort = this;
+        var $list = $('div.dropdown-content');
+        var $listLi = $('a',$list);
+        $listLi.sort(function(a, b){
+            var keyA = $(a).text();
+            var keyB = $(b).text();
+            if($($sort).hasClass('asc')){
+                return (keyA > keyB) ? 1 : 0;
+            } else {
+                return (keyA < keyB) ? 1 : 0;
+            }
+        });
+        $.each($listLi, function(index, row){
+            $list.append(row);
+        });
+        e.preventDefault();
+    });
