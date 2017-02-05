e876424abaab778baeca2308721716fdffbe3539
Eric Zhang <ekzhang1@gmail.com>
2017-02-04 20:18:47 -0600

    Fixed coding practices; integrated with bootstrap

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-$(function () {
-    var $list = $("div.dropdown-content");
-    var sorted = $("a", $list).toArray().sort(function (a, b) {
-        return b.textContent[0] < a.textContent[0]
-    });
-    $list.html(sorted);
-})
-
-$(function () {
-    var $list = $("ul.map_selector");
-    var sorted = $("li h3", $list).toArray().sort(function (a, b) {
-        return b.textContent[0] < a.textContent[0]
-    });
-    $list.html(sorted);
-})
+// Declarative JS to tinker with pages is bad practice.
+// I recommend using a MVC library such as React or AngularJS.
+// Anyway, that's besides the point for right now, since this is still a mockup.
